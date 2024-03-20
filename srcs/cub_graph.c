354103736e7e62218df8b1637d911a2620537a15
GabrielH <mac@localgabrihome.home>
2024-03-20 04:18:58 +0100

    add to doc , one is about  vectores the other is about angles, and ont graph lib to draw line in windos

--- srcs/cub_graph.c
+++ srcs/cub_graph.c
@@ -6,7 +6,7 @@
 /*   By: yugao <yugao@student.42madrid.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 02:51:23 by yugao             #+#    #+#             */
-/*   Updated: 2024/03/20 03:18:06 by yugao            ###   ########.fr       */
+/*   Updated: 2024/03/20 04:11:15 by yugao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ typedef struct s_info
 	int		map2d_x;
 	int		map2d_y;
 	int		map2d_size_unit;
+	int		color;
 	t_bool	up;
 	t_bool	down;
 	t_bool	left;
@@ -39,7 +40,7 @@ typedef struct s_point
 	double y;
 }   		t_point;
 
-void	graph_line(t_point p1, t_point p2, void *info)
+static void	graph_line(void *info, t_point p1, t_point p2)
 {
 	double	dx;
 	double	dy;
@@ -63,13 +64,47 @@ void	graph_line(t_point p1, t_point p2, void *info)
 	}
 }
 
-// int main() {
-// 	t_info info;
+void	graph_thick_line(void *info, t_point p1, t_point p2, double thick)
+{
+	double	mid;
+	int		i;
+
+	if (thick == 0)
+		thick = 1;
+	mid = (thick / 2);
+	i = 0;
+	while (i < 2 * mid)
+	{
+		graph_line (info,(t_point){p1.x - mid + i, p1.y - mid}, (t_point){p2.x - mid + i, p2.y - mid});
+		i ++;
+	}
+}
+
+void	graph_square(void *info, t_point p_center, int len_side)
+{
+	graph_thick_line (info, (t_point){p_center.x, p_center.y - (int)(len_side / 2 + 0.5)}, (t_point){p_center.x, p_center.y + (int)(len_side / 2 + 0.5)}, len_side);
+}
+
+void	graph_rectangle(void *info, t_point p_low_left, t_point p_up_right)
+{
+	int	i;
+
+	i = p_low_left.x;
+	while (i < p_up_right.x)
+	{
+		graph_thick_line (info, (t_point){p_low_left.x + i, p_low_left.y}, (t_point){p_low_left.x + i, p_up_right.y}, 1);
+		i ++;
+	}
+}
+int main() {
+	t_info info;
 	
-// 	info.mlx = mlx_init();
-// 	info.win = mlx_new_window(info.mlx, 800, 600, "Line Drawing");
+	info.mlx = mlx_init();
+	info.win = mlx_new_window(info.mlx, 800, 600, "Line Drawing");
 
-// 	graph_line((t_point){10, 100}, (t_point){5, 600}, &info);
-
-// 	mlx_loop(info.mlx);
-// }
+	graph_thick_line(&info, (t_point){250, 100}, (t_point){400, 600}, 10);
+	graph_line(&info, (t_point){0, 0}, (t_point){500, 600});
+	//graph_square(&info, (t_point){50, 100}, 50);
+	graph_rectangle(&info, (t_point){50, 50}, (t_point){300, 300});
+	mlx_loop(info.mlx);
+}
